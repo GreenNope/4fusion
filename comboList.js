3e0850ea8229830e03427d5f44c483f30c2a406b
GreenNope <tlash1000@gmail.com>
2018-09-09 20:59:26 -0400

    Create comboList.js

---
+++ comboList.js
@@ -0,0 +1,27 @@
+var combos = {
+  //base element x base element combos
+  steam: [{fire, water}]
+  magma: [{fire, earth}]
+  heat: [{fire, air}]
+  mud: [{earth, water}]
+  cloud: [{air, water}]
+  dust: [{earth, air}]
+  
+  pressure: [{air, air}]
+  
+  
+  //base element x tier 2 combos
+  
+  wind: [{pressure, air}]
+  
+  
+  //base x 3 combos
+  cold: [{wind, air}]
+  
+  
+  //base x 4 combos
+  storm: [{cold, cloud}]
+  
+
+
+}
